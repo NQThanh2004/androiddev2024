388a92f7c03560db6ea6ca7108456ebcb329fcee
NQThanh2004 <thanhnq.22bi13410@usth.edu.vn>
2024-09-28 22:42:28 +0700

    Practical 12

--- USTHWeather/app/src/main/java/vn/edu/usthweather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usthweather/WeatherActivity.java
@@ -1,12 +1,17 @@
 package vn.edu.usthweather;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -18,6 +23,7 @@ import com.google.android.material.tabs.TabLayoutMediator;
 
 public class WeatherActivity extends AppCompatActivity {
     private static final String TAG = "Weather";
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,39 +48,26 @@ public class WeatherActivity extends AppCompatActivity {
 
         MediaPlayer music = MediaPlayer.create(WeatherActivity.this, R.raw.sample);
         music.start();
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
 
     @Override
-    protected void onStart()
-    {
-        super.onStart();
-        Log.i(TAG, "Start");
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.bottom_bar_menu, menu);
+        return true;
     }
 
     @Override
-    protected void onResume()
-    {
-        super.onResume();
-        Log.i(TAG, "Resume");
-    }
-
-    @Override
-    protected void onPause()
-    {
-        super.onPause();
-        Log.i(TAG, "Pause");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.i(TAG, "Stop");
-    }
-
-    @Override
-    protected void onDestroy()
-    {
-        super.onDestroy();
-        Log.i(TAG, "Destroy");
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_setting) {
+            Intent intent = new Intent(this, PrefActivity.class);
+            this.startActivity(intent);
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
